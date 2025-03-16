6f562b7c80cb68a70af8b82d8c4b913a4bd4cc67
Wildan <wildannabil101@gmail.com>
2025-03-16 23:14:37 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,36 @@
 
 public class HaloGeneric
 {
-    public void SapaUser<T>(T user)
+   public void SapaUser<T>(T user)
+   {
+       Console.WriteLine($"Halo user {user}");
+   }
+}
+
+class Program
+{
+   static void Main()
+   {
+       HaloGeneric halo = new HaloGeneric();
+       halo.SapaUser("Wildan");
+   }
+}
+
+using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
     {
-        Console.WriteLine($"Halo user {user}");
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+
     }
 }
 
@@ -12,7 +39,8 @@ class Program
 {
     static void Main()
     {
-        HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser("Wildan");
+        DataGeneric<string> nimData = new DataGeneric<string>("103022300095");
+
+        nimData.PrintData();
     }
 }

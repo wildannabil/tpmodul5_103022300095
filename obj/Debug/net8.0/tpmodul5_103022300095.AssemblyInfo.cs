dbf20d64cd25cc90a897b4f29502065270e79139
wildan <wildannabil101@gmail.com>
2025-03-16 23:00:39 +0700

    Menambahkan class HaloGeneric dengan method SapaUser

--- obj/Debug/net8.0/tpmodul5_103022300095.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul5_103022300095.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_103022300095")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49397812cc383c34fe9102709cc6d41b36218083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ec8599a3001d90f155d4a1d67ecdb1ee94ff1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_103022300095")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_103022300095")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
